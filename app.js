78be50b93af2b23b2cebafd606b48e7b42fff7a6
Monika Cyprys <monko.projects@gmail.com>
2021-03-15 18:26:52 +0100

    create mobile-nav | add media queries

---
+++ app.js
@@ -0,0 +1,31 @@
+const nameSection = document.querySelectorAll(".name-list-nav");
+const submenu = document.querySelectorAll(".submenu");
+const menuBtn = document.querySelector(".hamburger-menu");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavOpen = document.querySelector(".menu-close");
+const mobileNavClose = document.querySelector(".menu-line ");
+
+function showMenu() {
+  if (!mobileNav.classList.contains("slide-in")) {
+    mobileNavOpen.style.display = "block";
+    mobileNavClose.style.display = "none";
+    mobileNav.classList.add("slide-in");
+  } else {
+    mobileNavClose.style.display = "block";
+    mobileNavOpen.style.display = "none";
+    mobileNav.classList.remove("slide-in");
+  }
+}
+function showSubmenu(e) {
+  let element = e.target;
+  if (element.classList.contains("name-list-nav")) {
+    if (!element.children[1].classList.contains("active")) {
+      element.children[1].classList.add("active");
+    } else element.children[1].classList.remove("active");
+  }
+}
+nameSection.forEach((section) => {
+  section.addEventListener("click", showSubmenu);
+}, false);
+
+menuBtn.addEventListener("click", showMenu);
