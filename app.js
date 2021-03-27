06e863e8c45a7c76363db00995cfc55e9fc5396b
Monika Cyprys <monko.projects@gmail.com>
2021-03-27 13:08:57 +0100

    fix fn for images and fn for nav

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 (function () {
   const nameSection = document.querySelectorAll(".name-list-nav");
   const submenu = document.querySelectorAll(".submenu");
+  const submenuLi = document.querySelectorAll(".submenu li");
   const menuBtn = document.querySelector(".hamburger-menu");
   const mobileNav = document.querySelector(".mobile-nav");
   const mobileNavOpen = document.querySelector(".menu-close");
   const mobileNavClose = document.querySelector(".menu-line ");
-  let elementIsClicked = false;
+
   function showMenu() {
     if (!mobileNav.classList.contains("slide-in")) {
       mobileNavOpen.style.display = "block";
@@ -20,37 +21,44 @@
   function showSubmenu(e) {
     let element = e.target;
     if (element.classList.contains("name-list-nav")) {
+      submenu.forEach((sub) => sub.classList.remove("active"));
+      submenuLi.forEach((li) =>
+        li.addEventListener("click", function () {
+          submenu.forEach((subm) => subm.classList.remove("active"));
+        })
+      );
       if (!element.children[1].classList.contains("active")) {
         element.children[1].classList.add("active");
-      } else if (elementIsClicked) {
+      } else {
         element.children[1].classList.remove("active");
-        elementIsClicked = false; //ttutaj
       }
     }
   }
-  function closeSubmenu() {
-    elementIsClicked = true;
-  }
+
   nameSection.forEach((section) => {
     section.addEventListener("click", showSubmenu);
   }, false);
 
   menuBtn.addEventListener("click", showMenu);
-  submenu.forEach((element) => {
-    element.addEventListener("click", closeSubmenu);
-  });
 })();
 
 (function () {
   const imgs = document.querySelectorAll(".image-section");
   let screenWidth = window.innerWidth;
-  if (screenWidth >= 950) {
-    imgs.forEach((img) => {
-      img.src = `/images/illustration-${img.dataset.desktop}.svg`;
-    });
-  } else if (screenWidth < 950) {
-    imgs.forEach((img) => {
-      img.src = `/images/illustration-${img.dataset.mobile}.svg`;
-    });
+
+  window.addEventListener("resize", checkScreenSize);
+  window.addEventListener("load", checkScreenSize);
+
+  function checkScreenSize() {
+    screenWidth = window.innerWidth;
+    if (screenWidth >= 970) {
+      imgs.forEach((img) => {
+        img.src = `/images/illustration-${img.dataset.desktop}.svg`;
+      });
+    } else if (screenWidth < 970) {
+      imgs.forEach((img) => {
+        img.src = `/images/illustration-${img.dataset.mobile}.svg`;
+      });
+    }
   }
 })();
